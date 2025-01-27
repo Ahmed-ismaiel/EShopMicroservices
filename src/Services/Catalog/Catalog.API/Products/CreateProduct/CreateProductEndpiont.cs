6d51679cfa8184532691b93061d8fa8592c6d1b0
Ahmed ismaiel elshaer <AElshaer@healthinsightsgroup.com>
2025-01-27 21:56:56 +0200

    1 - Created MapPost Minimal Api Which Takes Request , ISender Interface 2 - Made Command Which will be sent to the handler 3 - use ISender To send the Requset to the Handler 4 - Recieved The Resonce in the form of guid  5 - configured Mediator and Carter Libiraries  in program.cs 6 - used globale using

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpiont.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpiont.cs
@@ -1,4 +1,4 @@
-﻿using Carter;
+﻿
 
 namespace Catalog.API.Products.CreateProduct
 {
@@ -9,7 +9,34 @@ namespace Catalog.API.Products.CreateProduct
     {
         public void AddRoutes(IEndpointRouteBuilder app)
         {
-           Console.Write("Not Implemented");
+            /// <summary>
+            /// post /products - Create a new product 
+            /// takes request and Isender (which send req through mediator pipeline)
+
+
+            app.MapPost("/products", 
+                async (CreateProductRequest request, ISender sender) =>
+            {
+                // Command to create a new product
+                // b3ml map mn l data ly fel request le l object 
+                var command = request.Adapt<CreateProductCommand>();
+
+                // Send the command (Request ) through the mediator pipeline to be handled by the CreateProductCommandHandler
+                var result = await sender.Send(command);
+
+                // Adapt (Mapped) the result to the response
+                var response = result.Adapt<CreateProductResponse>();
+
+                // Return the response of newly created product
+                return Results.Created($"/products/{response.Id}", response);
+
+            })
+                .WithName("CreateProduct")
+                .Produces<CreateProductResponse>(201)
+                .ProducesProblem(400)
+                .WithSummary("Create a new product")
+                .WithDescription("Create a new product with the given data");
+
         }
     }
 }
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -2,11 +2,20 @@ var builder = WebApplication.CreateBuilder(args);
 
 //Add services to the container.
 
+builder.Services.AddCarter();
+
+
+builder.Services.AddMediatR(configuration =>
+{
+
+    configuration.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-app.MapGet("/", () => "Hello World!");
+app.MapCarter();
 
 app.Run();
