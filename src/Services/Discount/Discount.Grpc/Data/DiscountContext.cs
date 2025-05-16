c18726af93dcab7b637da98c5b6e23db8ea9b333
Ahmed ismaiel elshaer <AElshaer@healthinsightsgroup.com>
2025-05-16 11:08:01 +0300

    1 - Removed proto puff file and it service  2 - Explained Protopuff File  3 -  make discount Service which inherits   from  our proto puff file 4 - configured our db context and app settings and prog cs

---
+++ src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
@@ -0,0 +1,17 @@
+﻿using Discount.Grpc.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace Discount.Grpc.Data
+{
+    public class DiscountContext  : DbContext
+    {
+        public DiscountContext(DbContextOptions<DiscountContext> options) : base(options)
+        {
+        }
+        public virtual DbSet<Coupon> Coupons { get; set; } = default!;
+       
+
+
+    }
+}
